d6eeda18ee3d75336e66afbdcf3215ac46bc934f
Samir Hrnjic <samir.hrnjic@gmail.com>
2011-06-19 18:11:58 +0200

    Zavrseno sa izgledom i rutama

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#postavljanje title na stranicu
+	def title
+		base_title = "Ruby on Rails application simple"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
